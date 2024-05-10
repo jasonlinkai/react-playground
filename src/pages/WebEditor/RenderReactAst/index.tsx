cf1196246d700bf109e707f0d1c7b39099699dcb
Jacky <jacky@iyes.dev>
2024-05-10 18:06:37 +0800

    refact: RenderNode Component

--- src/pages/WebEditor/RenderReactAst/index.tsx
+++ src/pages/WebEditor/RenderReactAst/index.tsx
@@ -1,6 +1,6 @@
 import "./renderer.css";
 import clsx from "clsx";
-import React, { SyntheticEvent, useCallback, useState } from "react";
+import React, { SyntheticEvent, memo, useCallback, useState } from "react";
 import { AstNode, AstElement } from "./ast";
 import { useAst } from "../AstProvider";
 
@@ -18,93 +18,173 @@ interface RecursivlyRenderAstNodeProps {
   handleOnDragLeave: (ev: React.DragEvent, node: AstElement) => void;
   handleOnDrop: (ev: React.DragEvent, node: AstElement) => void;
 }
-const recursivlyRenderAstNode = ({
-  ast,
-  selectedAstElement,
-  editingSelectedAstElement,
-  dragOverAstElement,
-  handleOnClick,
-  handleOnDragStart,
-  handleOnDragOver,
-  handleOnDragLeave,
-  handleOnDrop,
-}: RecursivlyRenderAstNodeProps): JSX.Element | string => {
-  const isTextElement = "innerType" in ast;
-  // Base case: If the node is a text node, render it as is
-  if (isTextElement) {
-    return ast.content;
-  }
+// const recursivlyRenderAstNode = ({
+//   ast,
+//   selectedAstElement,
+//   editingSelectedAstElement,
+//   dragOverAstElement,
+//   handleOnClick,
+//   handleOnDragStart,
+//   handleOnDragOver,
+//   handleOnDragLeave,
+//   handleOnDrop,
+// }: RecursivlyRenderAstNodeProps): JSX.Element | string => {
+//   const isTextElement = "innerType" in ast;
+//   // Base case: If the node is a text node, render it as is
+//   if (isTextElement) {
+//     return ast.content;
+//   }
 
-  const node: AstElement = ast;
-  // Otherwise, it's an element node
-  const { uuid, type, props, children } = node;
-  const isRootElement = node.parentUuid === 'root';
-  const isSelectedElement =
-    selectedAstElement && selectedAstElement.uuid === uuid;
-  const isDragOverAstElement = dragOverAstElement && dragOverAstElement.uuid === node.uuid;
-  const draggable  = !isRootElement && isSelectedElement;
+//   const node: AstElement = ast;
+//   // Otherwise, it's an element node
+//   const { uuid, type, props, children } = node;
+//   const isRootElement = node.parentUuid === "root";
+//   const isSelectedElement =
+//     selectedAstElement && selectedAstElement.uuid === uuid;
+//   const isDragOverAstElement =
+//     dragOverAstElement && dragOverAstElement.uuid === node.uuid;
+//   const draggable = !isRootElement && isSelectedElement;
 
-  // register event for web-editor
-  const editorEventListeners: {
-    [key: string]: React.EventHandler<SyntheticEvent> | undefined;
-  } = {};
-  editorEventListeners.onClick = (e: React.MouseEvent) => {
-    handleOnClick(e, node);
-  };
-  if (draggable) {
-    editorEventListeners.onDragStart = (e: React.DragEvent) => {
-      handleOnDragStart(e);
-    };
-  } else {
-    editorEventListeners.onDragOver = (e: React.DragEvent) => {
-      handleOnDragOver(e, node);
-    };
-    editorEventListeners.onDragLeave = (e: React.DragEvent) => {
-      handleOnDragLeave(e, node);
-    };
-    editorEventListeners.onDrop = (e: React.DragEvent) => {
-      handleOnDrop(e, node);
-    };
-  }
+//   // register event for web-editor
+//   const editorEventListeners: {
+//     [key: string]: React.EventHandler<SyntheticEvent> | undefined;
+//   } = {};
+//   editorEventListeners.onClick = (e: React.MouseEvent) => {
+//     handleOnClick(e, node);
+//   };
+//   if (draggable) {
+//     editorEventListeners.onDragStart = (e: React.DragEvent) => {
+//       handleOnDragStart(e);
+//     };
+//   } else {
+//     editorEventListeners.onDragOver = (e: React.DragEvent) => {
+//       handleOnDragOver(e, node);
+//     };
+//     editorEventListeners.onDragLeave = (e: React.DragEvent) => {
+//       handleOnDragLeave(e, node);
+//     };
+//     editorEventListeners.onDrop = (e: React.DragEvent) => {
+//       handleOnDrop(e, node);
+//     };
+//   }
 
-  // Render the element with event listeners
-  const renderChildren = Array.isArray(children)
-    ? children.map((child) =>
-        recursivlyRenderAstNode({
-          ast: child,
-          selectedAstElement,
-          editingSelectedAstElement,
-          dragOverAstElement,
-          handleOnClick,
-          handleOnDragStart,
-          handleOnDragOver,
-          handleOnDragLeave,
-          handleOnDrop,
-        })
-      )
-    : children;
+//   // Render the element with event listeners
+//   const renderChildren = Array.isArray(children)
+//     ? children.map((child) =>
+//         recursivlyRenderAstNode({
+//           ast: child,
+//           selectedAstElement,
+//           editingSelectedAstElement,
+//           dragOverAstElement,
+//           handleOnClick,
+//           handleOnDragStart,
+//           handleOnDragOver,
+//           handleOnDragLeave,
+//           handleOnDrop,
+//         })
+//       )
+//     : children;
 
-  return React.createElement(
-    type,
-    {
-      ...props,
-      ...editorEventListeners,
-      draggable,
-      style: {
-        ...props.style,
-        ...(isSelectedElement ? editingSelectedAstElement?.props.style : {}), // 將編輯中的節點樣式覆蓋到畫面上
-      },
-      className: clsx([
-        props.className,
-        {
-          "selected-element": isSelectedElement,
-          "drag-over-element": isDragOverAstElement,
+//   return React.createElement(
+//     type,
+//     {
+//       ...props,
+//       ...editorEventListeners,
+//       draggable,
+//       style: {
+//         ...props.style,
+//         ...(isSelectedElement ? editingSelectedAstElement?.props.style : {}), // 將編輯中的節點樣式覆蓋到畫面上
+//       },
+//       className: clsx([
+//         props.className,
+//         {
+//           "selected-element": isSelectedElement,
+//           "drag-over-element": isDragOverAstElement,
+//         },
+//       ]),
+//     },
+//     renderChildren
+//   );
+// };
+const RenderNode: React.FC<RecursivlyRenderAstNodeProps> = memo(
+  ({ ast, ...p }) => {
+    console.log("ast.uuid", ast.uuid);
+    const {
+      selectedAstElement,
+      editingSelectedAstElement,
+      dragOverAstElement,
+      handleOnClick,
+      handleOnDragStart,
+      handleOnDragOver,
+      handleOnDragLeave,
+      handleOnDrop,
+    } = p;
+    const isTextElement = "innerType" in ast;
+    // Base case: If the node is a text node, render it as is
+    if (isTextElement) {
+      return ast.content;
+    }
+
+    const node: AstElement = ast;
+    // Otherwise, it's an element node
+    const { uuid, type, props, children } = node;
+    const isRootElement = node.parentUuid === "root";
+    const isSelectedElement =
+      selectedAstElement && selectedAstElement.uuid === uuid;
+    const isDragOverAstElement =
+      dragOverAstElement && dragOverAstElement.uuid === node.uuid;
+    const draggable = !isRootElement && isSelectedElement;
+
+    // register event for web-editor
+    const editorEventListeners: {
+      [key: string]: React.EventHandler<SyntheticEvent> | undefined;
+    } = {};
+    editorEventListeners.onClick = (e: React.MouseEvent) => {
+      handleOnClick(e, node);
+    };
+    if (draggable) {
+      editorEventListeners.onDragStart = (e: React.DragEvent) => {
+        handleOnDragStart(e);
+      };
+    } else {
+      editorEventListeners.onDragOver = (e: React.DragEvent) => {
+        handleOnDragOver(e, node);
+      };
+      editorEventListeners.onDragLeave = (e: React.DragEvent) => {
+        handleOnDragLeave(e, node);
+      };
+      editorEventListeners.onDrop = (e: React.DragEvent) => {
+        handleOnDrop(e, node);
+      };
+    }
+
+    // Render the element with event listeners
+    const renderChildren = Array.isArray(children) ? children : [children];
+
+    return React.createElement(
+      type,
+      {
+        ...props,
+        ...editorEventListeners,
+        draggable,
+        style: {
+          ...props.style,
+          ...(isSelectedElement ? editingSelectedAstElement?.props.style : {}), // 將編輯中的節點樣式覆蓋到畫面上
         },
-      ]),
-    },
-    renderChildren
-  );
-};
+        className: clsx([
+          props.className,
+          {
+            "selected-element": isSelectedElement,
+            "drag-over-element": isDragOverAstElement,
+          },
+        ]),
+      },
+      renderChildren.map((child) => {
+        return <RenderNode key={child.uuid} ast={child} {...p} />;
+      })
+    );
+  }
+);
 
 const RenderReactAST: React.FC = () => {
   const {
@@ -158,17 +238,17 @@ const RenderReactAST: React.FC = () => {
 
   return (
     <div id="ast-renderer">
-      {recursivlyRenderAstNode({
-        ast,
-        selectedAstElement,
-        editingSelectedAstElement,
-        handleOnClick,
-        handleOnDragStart,
-        handleOnDragOver,
-        handleOnDragLeave,
-        handleOnDrop,
-        dragOverAstElement,
-      })}
+      <RenderNode
+        ast={ast}
+        selectedAstElement={selectedAstElement}
+        editingSelectedAstElement={editingSelectedAstElement}
+        handleOnClick={handleOnClick}
+        handleOnDragStart={handleOnDragStart}
+        handleOnDragOver={handleOnDragOver}
+        handleOnDragLeave={handleOnDragLeave}
+        handleOnDrop={handleOnDrop}
+        dragOverAstElement={dragOverAstElement}
+      />
     </div>
   );
 };
